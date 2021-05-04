cb0700d228b410499dc56adc9e6646caa8ff86e6
Aleks Golubyatnikov <aleks.golubyatnikov@gmail.com>
2021-05-05 00:09:47 +0300

    init

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 chmod 0755 -R ./  
 
-./servers/1-create-admin-server.sh
-./servers/2-create-servers-for-k8s.sh
+./servers/1-create-admin-server.sh >> installation-admin.log
+./servers/2-create-servers-for-k8s.sh >> installation-k8s.log
