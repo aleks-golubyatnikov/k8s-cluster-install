f2efecc01202f723bb82380512d59897ed2fe76f
Aleks Golubyatnikov <aleks.golubyatnikov@gmail.com>
2021-05-05 01:31:27 +0300

    init

--- admin-scripts/install.sh
+++ admin-scripts/install.sh
@@ -3,4 +3,4 @@
 apt-get -y install sshpass >> install.log
 ansible-playbook -i ../ansible-playbook/hosts ../ansible-playbook/initial.yaml >> install.log
 ansible-playbook -i ../ansible-playbook/hosts ../ansible-playbook/iptables.yaml >> install.log
-#ansible-playbook -i ../ansible-playbook/hosts ../ansible-playbook/docker.yaml >> install.log
+ansible-playbook -i ../ansible-playbook/hosts ../ansible-playbook/docker.yaml >> install.log
